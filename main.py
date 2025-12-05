52a8e895d21be9f5661b2d22007f623e472a4310
Prahaladha-Reddy <prahaladreddy80@gmail.com>
2025-12-06 02:33:19 +0530

    allow cors origin

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",
+                   "https://*.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
